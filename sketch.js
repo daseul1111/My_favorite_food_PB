369cb4999fc62c2f87115a896558254cc8b3dded
daseul1111 <ektmf283@gmail.com>
2020-12-08 05:22:47 +0900

    2단계 1차커밋
    병을 아주 열심히 그렸으나 bezier를 이해 못해서 엉망진창

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-PImage bg;
+//PImage bg;
+let x =250;
+let y =250;
 
 //background setting
 function setup() {
-  bg = loadImage('pizza.jpg');
+  //bg = loadImage('pizza.jpg');
   createCanvas(500, 500);
 }
 
-function draw() {
-  background(bg);
+function draw () {
+  //background(bg);
+  background(220);
+
+  //draw beer
+  stroke(0);
+  strokeWeight(5);
+  line(x-5,y-60,x+5,y-60);
+  line(x-5,y-60,x-10,y-25);
+  line(x-10,y-25,x-20,y-20);
+  line(x-20,y-20,x-20,y+40);
+  bezier(x-20,y+40,0,x-50,y+50,0,x-20,y+50,0,x+20,y+40,0);
+  line(x+20,y+40,x+20,y-20);
+  line(x+20,y-20,x+10,y-25);
+  line(x+10,y-25,x+5,y-60);
 }
