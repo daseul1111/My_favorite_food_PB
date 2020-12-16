bbf5fd4565579d3872fe1ad7f53332dff58e1aff
daseul1111 <ektmf283@gmail.com>
2020-12-17 01:45:18 +0900

    2, 3-1 단계 1차 커밋
    병 그리기
    피자 사진 업로드하기

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 //PImage bg;
 let x =250;
 let y =250;
+let pizza;
 
+function preload() {
+  pizza = loadImage('pizza.jpg');
+}
 //background setting
 function setup() {
-  //bg = loadImage('pizza.jpg');
   createCanvas(500, 500);
 }
 
 function draw () {
   //background(bg);
   background(220);
+  image(pizza,0,0);
+  noLoop();
 
   //draw beer
   stroke(0);
@@ -19,7 +24,7 @@ function draw () {
   line(x-5,y-60,x-10,y-25);
   line(x-10,y-25,x-20,y-20);
   line(x-20,y-20,x-20,y+40);
-  bezier(x-20,y+40,0,x-50,y+50,0,x-20,y+50,0,x+20,y+40,0);
+  bezier(x-20,y+40,x-20,y+50,x+20,y+50,x+20,y+40);
   line(x+20,y+40,x+20,y-20);
   line(x+20,y-20,x+10,y-25);
   line(x+10,y-25,x+5,y-60);
