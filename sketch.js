a818be87f6268e1632dfd593e080f9d32ac4ba79
daseul1111 <ektmf283@gmail.com>
2020-12-17 02:38:24 +0900

    4단계 : 병의 개수 늘리기 1차 커밋
    var beer를 만들어줌

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 //PImage bg;
-let x =250;
-let y =250;
+var beer={x:250, y:250};
 let pizza;
 
 function preload() {
@@ -20,12 +19,15 @@ function draw () {
   //draw beer
   stroke(0);
   strokeWeight(5);
-  line(x-15,y-70,x+15,y-70);
-  line(x-15,y-70,x-20,y-35);
-  line(x-20,y-35,x-30,y-30);
-  line(x-30,y-30,x-30,y+50);
-  bezier(x-30,y+50,x-30,y+60,x+30,y+60,x+30,y+50);
-  line(x+30,y+50,x+30,y-30);
-  line(x+30,y-30,x+20,y-35);
-  line(x+20,y-35,x+15,y-70);
+  line(beer.x-10,beer.y-90,beer.x+15,beer.y-90); //병입구
+  line(beer.x-10,beer.y-90,beer.x-15,beer.y-35); //병 목 왼쪽
+  line(beer.x-15,beer.y-35,beer.x-25,beer.y-30);
+  line(beer.x-25,beer.y-30,beer.x-25,beer.y+50);
+  bezier(beer.x-25,beer.y+50,beer.x-30,beer.y+60,
+    beer.x+30,beer.y+60,beer.x+30,beer.y+50); //병 바닥
+  line(beer.x+30,beer.y+50,beer.x+30,beer.y-30);
+  line(beer.x+30,beer.y-30,beer.x+20,beer.y-35);
+  line(beer.x+20,beer.y-35,beer.x+15,beer.y-90); //병 목 오른쪽
+
+  beer.x = beer.x +1;
 }
